9d9abefb3b84b1614207f86e2abddff7479fb32b
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2017-12-24 23:45:50 -0600

    Finish Project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
-
-  // your code will go here
-
+  $(document).ready(function(){
+    $.ajax({
+      url: 'https://www.codeschool.com/users/rombo84fx.json',
+      dataType: 'jsonp',
+      success: function(response){
+          var completedCourses = $.map(response.courses.completed, function(course) {
+            var completedCourse = $('<div class="course"></div>');
+            var courseTitle = $('<h3>' + course.title + '</h3>');
+            var courseImage = $('<img src="' + course.badge + '"></img>');
+            var courseButton = $('<a href="' + course.url + '" target="_blank" class="btn btn-primary">See Course</a>');
+            completedCourse.html(courseTitle.append(courseImage).append(courseButton));
+            return completedCourse;
+          });
+          $('#badges').html(completedCourses);
+      }
+    });
+  });
 });
